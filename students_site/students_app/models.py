333866d06ed8cd80f9c87c8097392c0c2eb001af
Lilco <metra.lilco@gmail.com>
2023-10-01 21:29:05 +0300

    Add 'Teacher' model to 'models.py'

--- students_site/students_app/models.py
+++ students_site/students_app/models.py
@@ -6,3 +6,10 @@ class Student(models.Model):
     first_name = models.CharField(max_length=150, verbose_name="First name")
     last_name = models.CharField(max_length=200, verbose_name="Last name")
     birth_date = models.DateField(verbose_name="Date of birth")
+
+
+class Teacher(models.Model):
+    first_name = models.CharField(max_length=150, verbose_name="First name")
+    last_name = models.CharField(max_length=200, verbose_name="Last name")
+    birth_date = models.DateField(verbose_name="Date of birth")
+    subject = models.CharField(max_length=150, verbose_name="Subject")
