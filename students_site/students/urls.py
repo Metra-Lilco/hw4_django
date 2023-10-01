4a47b58fa52a405562701f63046aaca15cf8ef8e
Lilco <metra.lilco@gmail.com>
2023-10-01 14:54:08 +0300

    Create 'generate_student' and 'generate_students' functions

--- students_site/students/urls.py
+++ students_site/students/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("generate-student/", views.generate_student, name="generate_student"),
+    path("generate-students/", views.generate_students, 
+        name="generate_students")
 ]
